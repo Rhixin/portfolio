ad45d4a981a83a93c16814072cbd20c4339a1756
Rhixin <vallestedted@gmail.com>
2025-04-03 21:22:00 +0800

    changed home

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-"use client";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-
-export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push("/about");
-  }, [router]);
-
-  return null;
+export default function Home({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <>{children}</>;
 }
