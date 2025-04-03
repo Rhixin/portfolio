e7ef8b707d90b9933726d3f2cf955c6438eda9cb
Rhixin <vallestedted@gmail.com>
2025-04-03 21:29:09 +0800

    added dl button resume

--- app/(pages)/resume/page.tsx
+++ app/(pages)/resume/page.tsx
@@ -8,6 +8,14 @@ export default function Resume() {
         width="100%"
         height="500px"
       />
+
+      <a
+        href="/pdf/VallesCV.pdf"
+        download
+        className="mt-4 px-4 py-2 bg-[#373f51] text-white rounded-md"
+      >
+        Download PDF
+      </a>
     </div>
   );
 }
