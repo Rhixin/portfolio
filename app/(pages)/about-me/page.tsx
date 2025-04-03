85d96c9067a81c0849cc2dadabc7ab91c9a407c2
Rhixin <vallestedted@gmail.com>
2025-04-03 21:07:58 +0800

    Added i type

--- app/(pages)/about-me/page.tsx
+++ app/(pages)/about-me/page.tsx
@@ -74,7 +74,7 @@ export default function About() {
 
   const skillVariants = {
     hidden: { opacity: 0, y: 20 },
-    visible: (i) => ({
+    visible: (i: number) => ({
       opacity: 1,
       y: 0,
       transition: { delay: i * 0.1, duration: 1 },
