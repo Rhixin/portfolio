06132dacdc33f9bfc58e68fcd7661292912df35b
Rhixin <vallestedted@gmail.com>
2025-04-03 21:13:26 +0800

    manage strict

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Disables ESLint during build (for production only)
+    ignoreDuringBuilds: true,
+  },
+  // other config options here
 };
 
 export default nextConfig;
